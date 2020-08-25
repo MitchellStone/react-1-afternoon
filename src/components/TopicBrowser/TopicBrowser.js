9d351264bdbc3bc2e995bb644207895171f76686
mitchell stone <themitch.stone@gmail.com>
2020-08-25 14:39:56 -0600

    changed export default line to be above the render and return

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -2,15 +2,28 @@
 
 import React, {Component} from 'react';
 
+import EvenAndOdd from '../Topics/EvenAndOdd'
+import FilterObject from '../Topics/FilterObject'
+import FilterString from '../Topics/FilterString'
+import Palindrome from '../Topics/Palindrome'
+import Sum from '../Topics/Sum'
+
 //extends simply means that TopicBrowser will Inherit from React.Component
-class TopicBrowser extends Component {
+export default class TopicBrowser extends Component {
     render() {
-        return ( <p> Hello World </p>
+        return ( 
+            <div>
+            <EvenAndOdd/>
+            <FilterObject/>
+            <FilterString/>
+            <Palindrome/>
+            <Sum/>
+            </div>
         )
     }
 }
 
-export default TopicBrowser;
+
 
 
 // export default class MyNavbar extends React.Component {
