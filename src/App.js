6fc15cf3f516428de3f6e901c0aeff0866f7561d
mitchell stone <themitch.stone@gmail.com>
2020-08-25 14:19:50 -0600

    added import reach to all the Topic files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
 
+
+// render() {
+//   return [
+//     <ChildA key="key1" />,
+//     <ChildB key="key2" />,
+//     <ChildC key="key3" />,
+//   ];
+// }
 
 class App extends Component {
   render() {
+    return (
+      <TopicBrowser />
+    );
     
   }
 }
 
 export default App;
+
