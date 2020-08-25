aefcd572c43cf73f39b81790edb44e9e3e2db16c
mitchell stone <themitch.stone@gmail.com>
2020-08-25 17:31:32 -0600

    figuring out the constructor form for the this.state.

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -3,7 +3,25 @@ import React, { Component } from 'react'
 export default class EvenAndOdd extends Component {
     render(){
         return(
-            <p> EvenAndOdd Component </p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4> Even and Odds </h4>
+                <input className="inputLine"/>
+                <button className="confirmationButton"> Split </button>
+                <span className="resultBox"></span>
+                <span className="resultBox"></span>
+
+            </div>
+            
         )
     }
 }
+ 
+constructor() {
+     super();
+
+     this.state = {
+         evenArray: [],
+         oddArray: [],
+         userInput: ''
+     }
+ }
